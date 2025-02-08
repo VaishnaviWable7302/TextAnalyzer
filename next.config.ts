15dc590a8d4fed5e91ca062e82ca0f3e5fdcb54b
VaishnaviWable7302 <vaishnaviwable2020@gmail.com>
2025-02-08 18:12:47 +0530

    changing next.confing.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export", // Enables static export
-  basePath: "/TextAnalyzer", // Replace with your GitHub repo name
-  assetPrefix: "/TextAnalyzer/",
 };
 
 export default nextConfig;
-
-module.exports = nextConfig;
